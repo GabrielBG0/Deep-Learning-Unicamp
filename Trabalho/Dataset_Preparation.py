235663c9653e481f1fa74b1acb3df1f2ff9e53ed
Gabriel B Gutierrez <gabriel.bgs00@gmail.com>
2023-06-12 12:58:16 -0300

    update trabalho

--- Trabalho/Dataset_Preparation.py
+++ Trabalho/Dataset_Preparation.py
@@ -1,33 +1,39 @@
 import os
 import random
+import cv2
 
 random.seed(1234)
 
-with os.scandir("Dataset/Fish_Dataset") as subFolders:
+
+# read and resize a image to 256x256 and save to the same path
+def readAndResizeImage(oldPath, newPath):
+    img = cv2.imread(oldPath)
+    img = cv2.resize(img, (341, 256))
+    cv2.imwrite(newPath, img)
+
+
+with os.scandir("Dataset/organized_dataset") as subFolders:
     for id, imageClass in enumerate(subFolders):
-        with os.scandir(imageClass.path) as imagesAndMasks:
-            for folder in imagesAndMasks:
-                if "GT" in folder.name:
-                    with os.scandir(folder.path) as files:
-                        for fileId, file in enumerate(files):
-                            newPath = (
-                                "Dataset\Compiled\Classification\Mask\\"
-                                + "{:0>3}".format(id)
-                                + "_"
-                                + "{:0>5}".format(fileId)
-                                + "."
-                                + file.name.split(".")[1]
-                            )
-                            os.replace(file.path, newPath)
+        with os.scandir(imageClass.path) as classes:
+            for igmId, img in enumerate(classes):
+                if "_mask" in img.name:
+                    newPath = (
+                        "Dataset\Compiled\Classification\Mask\\"
+                        + "{:0>3}".format(id)
+                        + "_"
+                        + "{:0>5}".format(int(img.name.split("_")[0]) - 1)
+                        + "."
+                        + img.name.split(".")[1]
+                    )
+                    readAndResizeImage(img.path, newPath)
+
                 else:
-                    with os.scandir(folder.path) as files:
-                        for fileId, file in enumerate(files):
-                            newPath = (
-                                "Dataset\Compiled\Classification\Image\\"
-                                + "{:0>3}".format(id)
-                                + "_"
-                                + "{:0>5}".format(fileId)
-                                + "."
-                                + file.name.split(".")[1]
-                            )
-                            os.replace(file.path, newPath)
+                    newPath = (
+                        "Dataset\Compiled\Classification\Image\\"
+                        + "{:0>3}".format(id)
+                        + "_"
+                        + "{:0>5}".format(int(img.name.split(".")[0]) - 1)
+                        + "."
+                        + img.name.split(".")[1]
+                    )
+                    readAndResizeImage(img.path, newPath)
